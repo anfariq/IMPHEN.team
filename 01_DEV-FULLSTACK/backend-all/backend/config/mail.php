51a13cf9922d1590a74896bd3f58ccbb716895c5
Fariq <zeroy443@gmail.com>
2026-05-04 10:31:31 +0700

    update laravel

--- 01_DEV-FULLSTACK/backend-all/backend/config/mail.php
+++ 01_DEV-FULLSTACK/backend-all/backend/config/mail.php
@@ -47,6 +47,14 @@ return [
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url((string) env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
+
+            'stream' => [
+                'ssl' => [
+                    'allow_self_signed' => true,
+                    'verify_peer' => false,
+                    'verify_peer_name' => false,
+                ],
+            ],
         ],
 
         'ses' => [
@@ -69,6 +77,7 @@ return [
             'transport' => 'sendmail',
             'path' => env('MAIL_SENDMAIL_PATH', '/usr/sbin/sendmail -bs -i'),
         ],
+
 
         'log' => [
             'transport' => 'log',
