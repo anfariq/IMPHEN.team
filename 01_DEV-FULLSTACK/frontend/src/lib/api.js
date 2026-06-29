e1c834a9fc1ebc85702420290607ef486a4f647c
Fariq <zeroy443@gmail.com>
2026-06-29 18:14:56 +0700

    Change API base URL to new endpoint

--- 01_DEV-FULLSTACK/frontend/src/lib/api.js
+++ 01_DEV-FULLSTACK/frontend/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://imphenteam-production.up.railway.app/api',
+  baseURL: 'https://expressBack.ownspace.my.id/api',
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
